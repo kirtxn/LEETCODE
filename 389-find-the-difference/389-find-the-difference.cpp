8a3ef84bd7c1e1d31437cdd3c5a6fc9f256520a9
kirtxn <64327034+kirtxn@users.noreply.github.com>
2022-02-07 21:13:47 +0530

    Time: 7 ms (24.08%), Space: 6.8 MB (25.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -14,3 +14,11 @@ public:
         
     }
 };
+
+
+/* 
+(ONE LINER CODE)
+char findTheDifference(string s, string t) {
+	return accumulate(t.begin(), t.end(), 0) - accumulate(s.begin(), s.end(), 0);
+}
+*/
